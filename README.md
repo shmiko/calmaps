97fcdeb62afdbdf8d91bab5da1e8eb8e4807fbfd
Paul Jones <shmiko@gmail.com>
2015-10-31 23:31:23 +1100

    refactoring

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 CalMapIt - A MEAN Calendar Map Itinerary Application
+mkdir
+clone repo
+npm install
+bower install
+node app.js
--- server.js
+++ server.js
@@ -1,29 +1,36 @@
-(function () {
-    'use strict';
-    var express = require('express');
-    var app = express();
+// Import the Modules
+var express = require('express'),
+  bodyParser = require('body-parser')
+  logger = require('morgan')
+var path = require('path');
 
-    var app_config = require('./server/config/config_app.js');
-    var routes_config = require('./server/config/config_routes.js');
-    // config files
-    var db_config = require('./server/config/db');
-    // connect to our database
-    mongoose.connect(db_config.url);
-    // Check if MongoDB is running
-    mongoose.connection.on('error', function() {
-      console.error('MongoDB Connection Error. Make sureMongoDB is running.');
-    });
-    app.use('/', app_config);
-    app.use('/', routes_config);
-    app.use('/', db_config);
-    // START THE SERVER
-    console.log('STARTING THE API SERVER');
-    console.log('-------------------------');
-    app.listen(8080);
-    console.log('Started the API server');
-    process.on('uncaughtException', function (error) {
-        console.log(error.stack);
-        console.log(error);
-    });
+// configure app
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(logger('dev'));
 
-})();
+// where the application will run
+var port     = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/app'));
+// Defining the Routes file
+var routes = require('./server/routes/routes');
+// Use the routes file to get the routes
+app.use('/', routes);
+
+// view engine setup
+// app.use(express.static(__dirname + '/app'));
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
+
+// Set views path and view engine
+// app.use(express.static(__dirname + '/app'));
+ // app.set('view engine', 'app/index.html');
+ 
+// app.set('views', '/app/views');
+
+// Start the Node Server
+app.listen(port, function(){
+  console.log('Express server listening on port ',port);
+})
