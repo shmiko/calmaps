7539c3a7536b0d1095b84d4bb9f52c3b743c4fb8
Paul Jones <shmiko@gmail.com>
2015-10-31 12:34:47 +1100

    tests and api working

--- express.test.js
+++ express.test.js
@@ -1,6 +1,6 @@
 var superagent = require('superagent')
 var expect = require('expect.js')
-
+var debugging = false;
 
 
 describe('express rest api server testing', function(){
@@ -8,9 +8,12 @@ describe('express rest api server testing', function(){
   console.log("*************************")
   console.log("API TESTS STARTING")
   console.log("*************************")   
+  
   var id
   var coll = 'calevents';
   var std_console_output = coll + ". id is " + id + " response was ";
+  var coll2 = 'tests';
+  var std_console_output2 = coll2 + ". id is " + id + " response was ";
   
   it('post to ' + coll, function(done){
     superagent.post('http://localhost:8080/collections/' + coll)
@@ -18,45 +21,58 @@ describe('express rest api server testing', function(){
         , calevent_location: 'London'
       })
       .end(function(e,res){
-        console.log('post to ',std_console_output,res.body)
+        if (debugging) {
+          console.log('post to ',std_console_output,res.body)
+        }
         expect(e).to.eql(null)
         expect(res.body.length).to.eql(1)
         expect(res.body[0]._id.length).to.eql(24)
         id = res.body[0]._id
         done()
-        console.log('post to ' + coll,' completed!')
-        console.log("*************************")
-        console.log("")
+        if (debugging) {
+          console.log('post to ' + coll,' completed!')
+          console.log("*************************")
+          console.log("")
+        }  
       })    
   })
+
   
 
   it('gets a ' + coll, function(done){
     superagent.get('http://localhost:8080/collections/' + coll + '/' + id)
       .end(function(e, res){
-        console.log('gets a ',std_console_output,res.body)
+        if (debugging) {
+          console.log('gets a ',std_console_output,res.body)
+        }
         expect(e).to.eql(null)
         expect(typeof res.body).to.eql('object')
         expect(res.body._id.length).to.eql(24)        
         expect(res.body._id).to.eql(id)        
         done()
-        console.log('gets a ' + coll,' completed!')
-        console.log("*************************")
-        console.log("")
+        if (debugging) {
+          console.log('gets a ' + coll,' completed!')
+          console.log("*************************")
+          console.log("")
+        }
       })
   })
   
   it('retrieves all ' + coll, function(done){
     superagent.get('http://localhost:8080/collections/' + coll)
       .end(function(e, res){
-        console.log('retrieves all ',std_console_output,res.body)
+        if (debugging) {
+          console.log('retrieves all ',std_console_output,res.body)
+        }
         expect(e).to.eql(null)
         expect(res.body.length).to.be.above(0)
         expect(res.body.map(function (item){return item._id})).to.contain(id)        
         done()
-        console.log('gets all ' + coll,' completed!')
-        console.log("*************************")
-        console.log("")
+        if (debugging) {
+          console.log('gets all ' + coll,' completed!')
+          console.log("*************************")
+          console.log("")
+        }
       })
   })
   
@@ -65,53 +81,89 @@ describe('express rest api server testing', function(){
       .send({calevent_name: 'Christmas Holidays'
         , calevent_location: 'New York'})
       .end(function(e, res){
-        console.log('updates a ',std_console_output,res.body)
+        if (debugging) {
+          console.log('updates a ',std_console_output,res.body)
+        }
         expect(e).to.eql(null)
         expect(typeof res.body).to.eql('object')
         expect(res.body.msg).to.eql('success')        
         done()
-        console.log('updates a ' + coll,' completed!')
-        console.log("*************************")
-        console.log("")
+        if (debugging) {
+          console.log('updates a ' + coll,' completed!')
+          console.log("*************************")
+          console.log("")
+        }
       })
   })
   
   it('checks an updated ' + coll, function(done){
     superagent.get('http://localhost:8080/collections/' + coll + '/' + id)
       .end(function(e, res){
-        console.log('checks an updated ',std_console_output,res.body)
+        if (debugging) {
+          console.log('checks an updated ',std_console_output,res.body)
+        }
         expect(e).to.eql(null)
         expect(typeof res.body).to.eql('object')
         expect(res.body._id.length).to.eql(24)        
         expect(res.body._id).to.eql(id)        
         expect(res.body.calevent_name).to.eql('Christmas Holidays')        
         done()
-        console.log('checks an updated ' + coll,' completed!')
-        console.log("*************************")
-        console.log("")
+        if (debugging) {
+          console.log('checks an updated ' + coll,' completed!')
+          console.log("*************************")
+          console.log("")
+        }
       })
   })    
   
   it('removes a ' + coll, function(done){
     superagent.del('http://localhost:8080/collections/' + coll + '/' + id)
       .end(function(e, res){
-        console.log('removes a ',std_console_output,res.body)
+        if (debugging) {
+          console.log('removes a ',std_console_output,res.body)
+        }
         expect(e).to.eql(null)
         expect(typeof res.body).to.eql('object')
         expect(res.body.msg).to.eql('success')    
         done()
-        console.log('removes a ' + coll,' completed!')
-        console.log("")
-        console.log("*************************")
-        console.log("   API TESTS COMPLETED   ")
-        console.log("*************************")  
-        console.log("")
-        console.log("*************************")
-        console.log("  GENERIC TESTS STARTING ")
-        console.log("*************************")  
-        console.log("")
+        if (debugging) {
+          console.log('removes a ' + coll,' completed!')
+          console.log("")
+          console.log("*************************")
+          console.log("   API TESTS COMPLETED   ")
+          console.log("*************************")  
+          console.log("")
+          console.log("*************************")
+          console.log("  GENERIC TESTS STARTING ")
+          console.log("*************************")  
+          console.log("")
+        }
       })
   }) 
+
+  //Test another collection by using coll2 & std_console_output2 variables above
+  it('post to ' + coll2, function(done){
+    superagent.post('http://localhost:8080/collections/' + coll2)
+      .send({ calevent_name: 'Christmas'
+        , calevent_location: 'London'
+      })
+      .end(function(e,res){
+        if (debugging) {
+          console.log('post to ',std_console_output2,res.body)
+        }
+        expect(e).to.eql(null)
+        expect(res.body.length).to.eql(1)
+        expect(res.body[0]._id.length).to.eql(24)
+        id = res.body[0]._id
+        done()
+        if (debugging) {
+          console.log('post to ' + coll2,' completed!')
+          console.log("*************************")
+          console.log("")
+        }  
+      })    
+  })
+  
     
 })
 
@@ -124,14 +176,14 @@ describe('Array', function() {
     it('should return -1 when the value is not present', function () {
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
-      console.log("")
-      console.log("*************************")
-      console.log(" GENERIC TESTS COMPLETED ")
-      console.log("*************************")
+      if (debugging) {
+        console.log("")
+        console.log("*************************")
+        console.log(" GENERIC TESTS COMPLETED ")
+        console.log("*************************")
+      }
     })
-    
   })
-  
 });
 
 
---
+++ server/routes/routes.js
@@ -0,0 +1,52 @@
+var express = require('express'),
+	mongoskin = require('mongoskin');
+var router = express.Router();
+var db = mongoskin.db('mongodb://@localhost:27017/calmapit', {safe:true})
+
+
+router.param('collectionName', function(req, res, next, collectionName){
+  req.collection = db.collection(collectionName)
+  return next()
+})
+
+router.get('/', function(req, res, next) {
+  res.send('please select a collection, e.g., /collections/messages')
+})
+
+router.get('/collections/:collectionName', function(req, res, next) {
+  req.collection.find({} ,{limit: 10, sort: {'_id': -1}}).toArray(function(e, results){
+    if (e) return next(e)
+    res.send(results)
+  })
+})
+
+router.post('/collections/:collectionName', function(req, res, next) {
+  req.collection.insert(req.body, {}, function(e, results){
+    if (e) return next(e)
+    res.send(results)
+  })
+})
+
+router.get('/collections/:collectionName/:id', function(req, res, next) {
+  req.collection.findById(req.params.id, function(e, result){
+    if (e) return next(e)
+    res.send(result)
+  })
+})
+
+router.put('/collections/:collectionName/:id', function(req, res, next) {
+  req.collection.updateById(req.params.id, {$set: req.body}, {safe: true, multi: false}, function(e, result){
+    if (e) return next(e)
+    res.send((result === 1) ? {msg:'success'} : {msg: 'error'})
+  })
+})
+
+router.delete('/collections/:collectionName/:id', function(req, res, next) {
+  req.collection.removeById(req.params.id, function(e, result){
+    if (e) return next(e)
+    res.send((result === 1)?{msg: 'success'} : {msg: 'error'})
+  })
+})
+
+
+module.exports = router;
