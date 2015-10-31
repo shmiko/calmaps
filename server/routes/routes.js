5225440de5ee9e79dcb05e9b61a2caf526b38ae8
Paul Jones <shmiko@gmail.com>
2015-10-31 12:38:21 +1100

    mongoskin not mongoose

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,17 @@
-var express = require('express'),
-	mongoskin = require('mongoskin');
+var express = require('express');
+// Using mongoskin instead of mongoos as i do not want a schema forced
+var mongoskin = require('mongoskin');
 var router = express.Router();
 var db = mongoskin.db('mongodb://@localhost:27017/calmapit', {safe:true})
+
+// Use the below code to use mongoose instead
+// Import Mongoose
+//var mongoose   = require('mongoose');
+// connect to our database
+// you can use your own MongoDB installation at: 
+//mongoose.connect('mongodb://127.0.0.1/calmapit');
+//connect to remote
+//mongoose.connect('mongodb://username:password@kahana.mongohq.com:10073/node-api');
 
 
 router.param('collectionName', function(req, res, next, collectionName){
