4621293f3619475949c8681433b8e8e1ef07cd5d
Paul Jones <shmiko@gmail.com>
2015-11-01 11:19:41 +1100

    refactoring done.

--- server.js
+++ server.js
@@ -19,17 +19,6 @@ var routes = require('./server/routes/routes');
 // Use the routes file to get the routes
 app.use('/', routes);
 
-// view engine setup
-// app.use(express.static(__dirname + '/app'));
-// app.engine('html', require('ejs').renderFile);
-// app.set('view engine', 'html');
-
-// Set views path and view engine
-// app.use(express.static(__dirname + '/app'));
- // app.set('view engine', 'app/index.html');
- 
-// app.set('views', '/app/views');
-
 // Start the Node Server
 app.listen(port, function(){
   console.log('Express server listening on port ',port);
